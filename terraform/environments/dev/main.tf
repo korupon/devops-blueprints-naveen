5ae6bc653fd86ccebd1f41c96474fed38bc3e791
korupon <68062483+korupon@users.noreply.github.com>
2025-05-20 16:40:48 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,1 +1,25 @@
-# Placeholder for main.tf
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-dev-demoapp"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-dev-demoapp"
+  location            = azurerm_resource_group.dev.location
+  resource_group_name = azurerm_resource_group.dev.name
+  dns_prefix          = "devdemoapp"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
